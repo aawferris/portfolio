7dad94153455425fee439c3a4b98da914b82fee3
Andrew <aawferris@gmail.com>
2020-12-09 13:44:46 -0500

    added contact form

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import Home from "./screens/Home/Home";
 import Layout from "./components/shared/Layout/Layout";
 import About from "./screens/About/About";
+import Contact from "./screens/Contact/Contact";
 import Portfolio from "./screens/Portfolio/Portfolio";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <Layout>
         <Switch>
+          <Route path="/contact" component={Contact} />
           <Route path="/about" component={About} />
           <Route path="/portfolio" component={Portfolio} />
           <Route exact path="/" component={Home} />
