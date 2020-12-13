5649df8dbd8cf09859d41507f5c11fd5d9b47080
Andrew <aawferris@gmail.com>
2020-12-13 18:44:40 -0500

    restyled the projects page with cards

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Layout from "./components/shared/Layout/Layout";
 import About from "./screens/About/About";
 import Contact from "./screens/Contact/Contact";
 import DetailsProject from "./screens/DetailsProject/DetailsProject";
-import Portfolio from "./screens/Portfolio/Portfolio";
+// import Portfolio from "./screens/Portfolio/Portfolio";
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
           <Route path="/contact" component={Contact} />
           <Route path="/about" component={About} />
           <Route path="/details" component={DetailsProject} />
-          <Route path="/portfolio" component={Portfolio} />
+          {/* <Route path="/portfolio" component={Portfolio} /> */}
           <Route exact path="/" component={Home} />
         </Switch>
       </Layout>
--- client/src/screens/DetailsProject/DetailsProject.jsx
+++ client/src/screens/DetailsProject/DetailsProject.jsx
@@ -1,145 +1,80 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
-
+import React from 'react';
+import {Link} from 'react-router-dom'
 import './DetailsProject.css'
 
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 345,
-  },
-  media: {
-    height: 140,
-  },
-});
-
-export default function DetailsProject() {
-  const classes = useStyles();
+function DetailsProject(props) {
   return (
-    <div id='main-project-div'>
-      <div id="card1" className='main-card-container'>
-        <Card className='card-container' className={classes.root}>
-        <CardActionArea>
-            <CardMedia
-            className={classes.media}
-            id="card-image"
-            image="/assets/86list.png"
-            title="Main Page of Site"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-                86List
-            </Typography>
-            <Typography gutterBottom component="h5">
-              React.js, Ruby on Rails, Postgresql, CSS
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-            86List is a community for service-industry professionals to talk about the clients that they serve. 
-                Built with Ruby on Rails and React.js, 86List requires login authentication and registration in order to 
-                view and interact with co-workers' posts. In future iterations, I want to allow users to create their own
-                accounts and request to be a part of a community with a community leader's approval.
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-        <a href="https://86list.netlify.app/" target="_blank" rel="noopener"><img src="/assets/internet.png" className="deploy-link" /></a>
-        <a href="https://github.com/aawferris/86list" target="_blank" rel="noopener"><img src="/assets/github-logo.png" className="repo-link"/></a>
-        </CardActions>
-          </Card>
+    <div className='projects-container'>
+      <div class="flip-card">
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
+            <img className="image" src="/assets/86list.png" alt="86list site" />
+          </div>
+          <div class="flip-card-back">
+            <h1 className="back-title">86List</h1>
+            <p className="back-tools">React.js, Ruby on Rails, Postgresql, CSS</p>
+            <p className="back-description">86List is a community for service-industry professionals to talk about the clients that they serve. 
+              Built with Ruby on Rails and React.js, 86List requires login authentication and registration in order to 
+              view and interact with co-workers' posts. In future iterations, I want to allow users to create their own
+              accounts and request to be a part of a community with a community leader's approval.</p>
+            <a className="project-link" href="https://86list.netlify.app/" alt="deployed app" target="_blank" rel="noopener">Repo</a>
+            <a className="project-link" href="https://github.com/aawferris/86list" alt="Git Hub repo" target="_blank" rel="noopener">Site</a>
+          </div>
+        </div>
+        <div>
+        </div>
       </div>
-      
-      <div id="card2" className='main-card-container'>
-        <Card className={classes.root}>
-        <CardActionArea>
-          <CardMedia
-            className={classes.media}
-            image="/assets/teacher-source.png"
-            title="Main Page of Site"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-                Teacher Source
-            </Typography>
-            <Typography gutterBottom component="h5">
-            React.js, Express.js, MongoDB, CSS
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-            Teacher Source is the result of a cross-discipline collaboration with a UX team using Agile workflow. 
-              My team of programmers and I took the UX Team's designs and brought them to life. This project uses MongoDB
-              and React.js to create a dashboard for teachers to upload and share lesson planning resources.  The Team
-              and I have decided to continue to work on this project together.
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-            <a href="https://teacher-source.netlify.app/" target="_blank" rel="noopener"><img src="/assets/internet.png" className="deploy-link" /></a>
-            <a href="https://github.com/aawferris/teachersource" target="_blank" rel="noopener"><img src="/assets/github-logo.png" className="repo-link"/></a>
-        </CardActions>
-          </Card>
+      <div class="flip-card">
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
+            <img className="image" src="/assets/teachersource.png" alt="Teacher Source site" />
+          </div>
+          <div class="flip-card-back">
+            <h1 className="back-title">Teacher Source</h1>
+            <p className="back-tools">React.js, Express.js, MongoDB, CSS</p>
+            <p className="back-description">Teacher Source is the result of a cross-discipline collaboration with a UX team using Agile workflow.  
+             My team of programmers and I took the UX Team's designs and brought them to life. This project uses MongoDB
+            and React.js to create a dashboard for teachers to upload and share lesson planning resources.  The Team
+              and I have decided to continue to work on this project together.</p>
+              <a className="project-link" href="https://teacher-source.netlify.app/" alt="deployed app" target="_blank" rel="noopener">Repo</a>
+              <a className="project-link" href="https://github.com/aawferris/teachersource" alt="Git Hub repo" target="_blank" rel="noopener">Site</a>
+          </div>
+        </div>
       </div>
-      
-      <div id="card3" className='main-card-container'>
-        <Card className={classes.root}>
-        <CardActionArea>
-          <CardMedia
-            className={classes.media}
-            image="/assets/productively.png"
-            title="Main Page of Site"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-                Productive.ly
-            </Typography>
-            <Typography gutterBottom component="h5">
-            React.js, Express.js, CSS
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-            Productive.ly is a React.js app that leverages AirTable API to allow users to stay focused and
-               get work done.  This app has a place for note-taking and task lists.  I want to make this app
-              into a browser extension.
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <a href="https://productively.netlify.app/" target="_blank" rel="noopener"><img src="/assets/internet.png" className="deploy-link" /></a>
-          <a href="https://github.com/aawferris/productive.ly" target="_blank" rel="noopener"><img src="/assets/github-logo.png" className="repo-link"/></a>
-        </CardActions>
-          </Card>
+      <div class="flip-card">
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
+            <img className="image" src="/assets/productively.png" alt="Productive.ly site" />
+          </div>
+          <div class="flip-card-back">
+            <h1 className="back-title">Productive.ly</h1>
+            <p className="back-tools">React.js, Express.js, CSS</p>
+            <p className="back-description">Productive.ly is a React.js app that leverages AirTable API to allow users to stay focused and
+            get work done.  This app has a place for note-taking and task lists.  I want to make this app
+              into a browser extension.</p>
+              <a className="project-link" href="https://productively.netlify.app/" alt="deployed app" target="_blank" rel="noopener">Repo</a>
+              <a className="project-link" href="https://github.com/aawferris/productive.ly" alt="Git Hub repo" target="_blank" rel="noopener">Site</a>
+          </div>
+        </div>
       </div>
-
-      <div id="card4" className='main-card-container'>
-        <Card className={classes.root}>
-        <CardActionArea>
-          <CardMedia
-            className={classes.media}
-            image="/assets/cia.png"
-            title="Main Page of Site"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-                CIA Cover Story Generator
-            </Typography>
-            <Typography gutterBottom component="h5">
-            Vanilla Javascript, CSS, HTML5
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-            This is the first site I ever programmed using code. I used Javascript as well as made 
-               axios calls to RESTFUL APIs to to create this silly random person generator, styled to be a 
-              tool for CIA operatives to generate a cover story.
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <a href="https://aawferris.github.io/CIA-cover-story/index.html" target="_blank" rel="noopener"><img src="/assets/internet.png" alt="link to github" className="deploy-link" /></a>
-          <a href="https://github.com/aawferris/CIA-cover-story" target="_blank" rel="noopener"><img src="/assets/github-logo.png" alt="link to deployment" className="repo-link"/></a>
-        </CardActions>
-          </Card>
+      <div class="flip-card">
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
+            <img className="image" src="/assets/cia.png" alt="CIA Cover Story Generator site" />
+          </div>
+          <div class="flip-card-back">
+            <h1 className="back-title">CIA Cover Story Generator</h1>
+            <p className="back-tools">Vanilla Javascript, CSS, HTML5</p>
+            <p className="back-description">This is the first site I ever programmed using code. I used Javascript as well as made 
+            axios calls to RESTFUL APIs to to create this silly random person generator, styled to be a 
+              tool for CIA operatives to generate a cover story.</p>
+              <a className="project-link" href="https://aawferris.github.io/CIA-cover-story/index.html" alt="deployed app" target="_blank" rel="noopener">Repo</a>
+              <a className="project-link" href="https://github.com/aawferris/CIA-cover-story" alt="Git Hub repo" target="_blank" rel="noopener">Site</a>
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
+
+export default DetailsProject;
